4803e50d7254d92f65ac44163745bc947d45fe49
me Alan Wolf <alan_wolf@me.com>
2016-03-08 14:33:16 -0500

    Initial Commit

--- Timed Fetch/ViewController.swift
+++ Timed Fetch/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer = NSTimer()
+    var myCounter = 0
+    
+    
+    @IBOutlet weak var latLabel: UILabel!
+    @IBOutlet weak var lonLabel: UILabel!
+    @IBOutlet weak var myLabel: UILabel!
+    func incrementCounter(myTimer:NSTimer){
+     
+        myCounter = myCounter + 1
+        myLabel.text = String(myCounter)
+        
+        
+        
+        
+    }
+    
+    @IBAction func startFetching(sender: AnyObject) {
+        
+        if timer.valid == false {
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval (0.1, target: self, selector: ("incrementCounter:"), userInfo: nil, repeats: true)
+            print ("timer is on")
+             print(myCounter)
+        }
+    }
+    
+    @IBAction func stopFetching(sender: AnyObject) {
+        
+        if timer.valid == true{
+            timer.invalidate()
+             print(myCounter)
+            print ("timer is off")
+        }
+        
+    }
+    
+    @IBAction func reset(sender: AnyObject) {
+        
+        myCounter = 0
+        myLabel.text = String(myCounter)
+        print(myCounter)
+        print("timer reset")
+    }
+    
+      
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
